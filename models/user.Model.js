fe5baa251f13a9e4df21712cfa883e7392aa092c
galeonn321 <santichiste@hotmail.com>
2024-08-05 01:27:49 +0200

    small update

--- models/user.Model.js
+++ models/user.Model.js
@@ -2,84 +2,84 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-  userId: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  username: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  password: {
-    type: String,
-    required: true,
-  },
-  profilePicture: {
-    type: String,
-  },
-  comments: [
-    {
-      movieId: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Movie',
-        required: true,
-      },
-      comment: {
+    userId: {
         type: String,
         required: true,
-      },
-      date: {
-        type: Date,
-        default: Date.now,
-      }
-    }
-  ],
-  likes: [
-    {
-      movieId: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Movie',
+        unique: true,
+    },
+    username: {
+        type: String,
         required: true,
-      },
-      date: {
-        type: Date,
-        default: Date.now,
-      }
-    }
-  ],
-  savedMovies: [
-    {
-      movieId: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Movie',
+        unique: true,
+    },
+    email: {
+        type: String,
         required: true,
-      },
-      date: {
-        type: Date,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    profilePicture: {
+        type: String,
+    },
+    comments: [
+        {
+            movieId: {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: 'Movie',
+                required: true,
+            },
+            comment: {
+                type: String,
+                required: true,
+            },
+            date: {
+                type: Date,
+                default: Date.now,
+            }
+        }
+    ],
+    likes: [
+        {
+            movieId: {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: 'Movie',
+                required: true,
+            },
+            date: {
+                type: Number,
+                default: Date.now,
+            }
+        }
+    ],
+    savedMovies: [
+        {
+            movieId: {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: 'Movie',
+                required: true,
+            },
+            date: {
+                type: Number,
+                default: Date.now,
+            }
+        }
+    ],
+    createdAt: {
+        type: Number,
         default: Date.now,
-      }
+    },
+    updatedAt: {
+        type: Number,
+        default: Date.now,
     }
-  ],
-  createdAt: {
-    type: Date,
-    default: Date.now,
-  },
-  updatedAt: {
-    type: Date,
-    default: Date.now,
-  }
 });
 
 userSchema.pre('save', function (next) {
-  this.updatedAt = Date.now();
-  next();
+    this.updatedAt = Date.now();
+    next();
 });
 
 const UserModel = mongoose.model('User', userSchema);
